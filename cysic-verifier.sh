69f5cd9fc21e169cd2eecc5a58d0ef13e46d8cb9
Billy Junaidi <billy.junaidi@gmail.com>
2025-01-01 03:04:09 +0800

    include prover

--- cysic-verifier.sh
+++ cysic-verifier.sh
@@ -10,25 +10,29 @@ REWARD_ADDRESS=""
 # Check if the shell is using bash
 shell_check
 
-install() {
+docker_check(){
 # Check and install docker if not available
 if ! command -v docker &> /dev/null; then
-    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -qy $pkg; done
-    sudo apt update -qy
-    sudo apt -qy install curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update -qy
-    sudo apt install -qy docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo systemctl enable --now docker
-    sudo usermod -aG docker $USER
-    msg_ok "Docker has been installed."
+  msg_info "Installing docker..."
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -qy $pkg; done
+  sudo apt update -qy
+  sudo apt -qy install curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update -qy
+  sudo apt install -qy docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo systemctl enable --now docker
+  sudo usermod -aG docker $USER
 fi
+msg_ok "Docker has been installed."
+}
+install_verifier() {
+docker_check
 
 mkdir -p $HOME/cysic-verifier
 cd $HOME/cysic-verifier
@@ -52,7 +56,7 @@ services:
     build: .
     restart: unless-stopped
     volumes:
-      - ${PWD}/data/.cysic:/root/.cysic
+      - ${PWD}/data/cysic:/root/.cysic
       - ${PWD}/data/app:/app/data
     environment:
       - REWARD_ADDRESS=${REWARD_ADDRESS}
@@ -70,27 +74,6 @@ cd /root/cysic-verifier
 bash ./start.sh
 EOF
 msg_ok "Cysic-Verifier has been installed."
-}
-
-if (whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" --yesno "This script will install the Cysic-Verifier. Do you want to continue?" 10 60); then
-    while [ -z "$REWARD_ADDRESS" ]; do
-      if REWARD_ADDRESS=$(whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" --inputbox "Input your reward address (EVM starting with 0x):" 8 60 3>&1 1>&2 2>&3); then
-        if [[ $REWARD_ADDRESS != 0x* ]]; then
-            whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" --msgbox "Error: Reward Address must start with 0x" 8 60
-            REWARD_ADDRESS=""
-        elif (whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" --yesno "\nReward Address: $REWARD_ADDRESS\n\nContinue with the installation?" 10 60); then
-            break
-        else
-            REWARD_ADDRESS=""
-        fi
-      else
-        exit_script
-      fi
-    done
-    install
-else
-    exit_script
-fi
 
 if (whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" --yesno "Do you want to run the Cysic-Verifier?" 10 60); then
     if [ "$(docker ps -q -f name=cysic-verifier --no-trunc | wc -l)" -ne "0" ]; then
@@ -110,9 +93,96 @@ if (whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" 
     sudo docker compose -f $HOME/cysic-verifier/docker-compose.yml up -d >/dev/null 2>&1
     msg_ok "Cysic-Verifier started successfully.\n"
 fi
-
 echo -e "${ROOTSSH}${YW} Please backup your Cysic-Verifier keys folder. '$HOME/cysic-verifier/data/keys' to prevent data loss.${CL}"
 echo -e "${INFO}${GN} To start Cysic-Verifier, run the command: 'docker compose -f $HOME/cysic-verifier/docker-compose.yml up -d'${CL}"
 echo -e "${INFO}${GN} To stop Cysic-Verifier, run the command: 'docker compose -f $HOME/cysic-verifier/docker-compose.yml down'${CL}"
 echo -e "${INFO}${GN} To restart Cysic-Verifier, run the command: 'docker compose -f $HOME/cysic-verifier/docker-compose.yml restart'${CL}"
 echo -e "${INFO}${GN} To check the logs of Cysic-Verifier, run the command: 'docker compose -f $HOME/cysic-verifier/docker-compose.yml logs -fn 100'${CL}"
+}
+
+install_prover() {
+docker_check
+
+mkdir -p $HOME/cysic-prover
+cd $HOME/cysic-prover
+
+tee Dockerfile > /dev/null << EOF
+FROM ubuntu:noble
+
+RUN apt update -qq && \\
+    apt install -y -qq curl ca-certificates libc6 && \\
+    curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/setup_prover.sh > /root/setup_prover.sh
+
+COPY entrypoint.sh .
+RUN chmod +x entrypoint.sh
+
+CMD ["./entrypoint.sh"]
+EOF
+tee entrypoint.sh > /dev/null << EOF
+#!/bin/sh
+bash ./root/setup_prover.sh
+cd /root/cysic-prover
+bash ./start.sh
+EOF
+tee docker-compose.yml > /dev/null << EOF
+services:
+  cysic-prover:
+    container_name: cysic-prover
+    build: .
+    restart: unless-stopped
+    volumes:
+      - ${PWD}/data/cysic:/root/.cysic
+      - ${PWD}/data/app:/app/data
+      - ${PWD}/data/scroll_prover:/root/.scroll_prover
+    deploy:
+      resources:
+        reservations:
+          devices:
+            - driver: "nvidia"
+              count: "all"
+              capabilities: ["gpu"]
+    environment:
+      - REWARD_ADDRESS=${REWARD_ADDRESS}
+    stop_grace_period: 1m
+    network_mode: host
+EOF
+msg_ok "Cysic-Prover has been installed."
+}
+init_cysic() {
+if (whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-${1}" --yesno "This script will install the Cysic-${1}. Do you want to continue?" 10 60); then
+    while [ -z "$REWARD_ADDRESS" ]; do
+      if REWARD_ADDRESS=$(whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-${1}" --inputbox "Input your reward address (EVM starting with 0x):" 8 60 3>&1 1>&2 2>&3); then
+        if [[ $REWARD_ADDRESS != 0x* ]]; then
+            whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-${1}" --msgbox "Error: Reward Address must start with 0x" 8 60
+            REWARD_ADDRESS=""
+        elif (whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-${1}" --yesno "\nReward Address: $REWARD_ADDRESS\n\nContinue with the installation?" 10 60); then
+            break
+        else
+            REWARD_ADDRESS=""
+        fi
+      else
+        exit_script
+      fi
+    done
+    install_${1}
+else
+    exit_script
+fi
+}
+
+while true; do
+    choice=$(whiptail --backtitle "CryptoNodeID Helper Scripts" --title "Cysic-Verifier" --menu "Choose the type of Cysic-Verifier to install:" 10 60 2 \
+        "Verifier" "Install the Cysic-Verifier (Default)" \
+        "Prover" "Install the Cysic-Prover" 3>&1 1>&2 2>&3)
+    case $choice in
+        "Verifier")
+            init_cysic "verifier"
+            break
+            ;;
+        "Prover")
+            init_cysic "prover"
+            break
+            ;;
+    esac
+done
+
